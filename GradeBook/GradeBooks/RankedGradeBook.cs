6ef2f2f07e68d22bfcedd03131c26aa58c7f74ec
Malik Herring <malikherring@u.boisestate.edu>
2019-05-30 11:51:47 -0600

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GradeBook.GradeBooks
+﻿namespace GradeBook.GradeBooks
 {
   public class RankedGradeBook : BaseGradeBook
   {
     public RankedGradeBook(string name) : base(name)
     {
-      Type = Enums.GradeBookType.Ranked; 
+      Type = Enums.GradeBookType.Ranked;
     }
   }
 }
